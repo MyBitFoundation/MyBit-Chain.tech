92edc222cd70d12d7767b871219147a04b97b103
Iggy Grey <github@pmphillips.ca>
2018-10-01 16:02:10 -0700

    js wrapper for contract addresses

---
+++ index.js
@@ -0,0 +1,69 @@
+var fs = require("fs");
+var path = require('path');
+var appRoot = path.resolve(__dirname);
+
+module.exports = (function (){
+  var addresses = JSON.parse(fs.readFileSync(appRoot + '/addresses.json'));
+
+  return {
+    MyBit: function(){
+      return addresses.MyBit;
+    },
+
+    ERC20Burner: function(){
+      return addresses.ERC20Burner;
+    },
+
+    Database: function(){
+      return addresses.Database;
+    },
+
+    ContractManager: function(){
+      return addresses.ContractManager;
+    },
+
+    SingleOwned: function(){
+      return addresses.SingleOwned;
+    },
+
+    Pausible: function(){
+      return addresses.Pausible;
+    },
+
+    AccessHierarchy: function(){
+      return addresses.AccessHierarchy;
+    },
+
+    PlatformFunds: function(){
+      return addresses.PlatformFunds;
+    },
+
+    Operators: function(){
+      return addresses.Operators;
+    },
+
+    BrokerEscrow: function(){
+      return addresses.BrokerEscrow;
+    },
+
+    CrowdsaleETH: function(){
+      return addresses.CrowdsaleETH;
+    },
+
+    CrowdsaleGeneratorETH: function(){
+      return addresses.CrowdsaleGeneratorETH;
+    },
+
+    CrowdsaleERC20: function(){
+      return addresses.CrowdsaleERC20;
+    },
+
+    CrowdsaleGeneratorERC20: function(){
+      return addresses.CrowdsaleGeneratorERC20;
+    },
+
+    AssetExchange: function(){
+      return addresses.AssetExchange;
+    }
+  }
+})();
