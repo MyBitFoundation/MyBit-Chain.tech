fcd5c56df5917726bb5b75e60130b56f1874e50b
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-10-12 13:28:40 -0500

    Updating chain.js to actually use the data dir to start ganache

--- chain.js
+++ chain.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env node
 var ganache = require("ganache-cli");
+var dataDir = `${__dirname}/chain`;
+
 var server = ganache.server({
   'network_id': 70,
   'total_accounts': 20,
-  'db_path': 'chain',
+  'db_path': dataDir,
   'mnemonic': 'myth like bonus scare over problem client lizard pioneer submit female collect'
 })
 server.listen(8545, (err, blockchain) => {
